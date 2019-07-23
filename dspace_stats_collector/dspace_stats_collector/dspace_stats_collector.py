3d0825f5e6906d7e92f4d9ba9de98c9547023872
Lautaro Matas <lmatas@gmail.com>
2019-07-23 13:56:08 +0200

    Basic Event pipeline implementation

--- dspace_stats_collector/dspace_stats_collector/dspace_stats_collector.py
+++ dspace_stats_collector/dspace_stats_collector/dspace_stats_collector.py
@@ -1,3 +1,77 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+class Event:
+
+    _data_dict = {}
+
+    def __init__(self):
+        None        
+
+    def __getattr__(self, attribute):
+        return self._data_dict[attribute]
+
+    def __setattr__(self, name, value):
+        self._data_dict[name] = value
+
+    def __str__(self):
+        return self._data_dict.__str__()
+    
+
+
+class EventPipeline:
+
+    _input_stage = None
+    _filters_stage = []
+    _output_stage = None
+
+    def __init__(self, input, filters, output):
+        self._input_stage = input
+        self._filters_stage = filters
+        self._output_stage = output
+
+    def run(self):
+        events = self._input_stage.run() 
+
+        for filter in self._filters_stage:
+            events = filter.run(events)
+
+        self._output_stage.run(events)
+
+
+class DummyInput:
+
+    def __init__(self):
+        None
+        
+    def run(self):
+        for x in range(1,50 ):
+            e = Event()
+            e.id = '00' + str(x)
+            yield e
+
+
+class DummyFilter:
+
+    def __init__(self):
+        None
+        
+    def run(self, events):
+        print(events)
+
+        for event in events:
+            event.url = "http://dummy.org/" + event.id
+            yield event
+
+class DummyOutput:
+
+    def __init__(self):
+        None
+        
+    def run(self, events):
+        for event in events:
+            print(event)
+
+
+dummy_pipeline = EventPipeline(DummyInput(), [DummyFilter()], DummyOutput())
--- dspace_stats_collector/tests/test_dspace_stats_collector.py
+++ dspace_stats_collector/tests/test_dspace_stats_collector.py
@@ -9,17 +9,15 @@ import pytest
 from dspace_stats_collector import dspace_stats_collector
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     # import requests
+#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_dummy_pipeline():
+     dspace_stats_collector.dummy_pipeline.run()
