66a0684fb61447cd38183f3488e1ec7d9cf801c6
César Olivares <colivares@concytec.gob.pe>
2019-08-20 23:19:50 -0500

    75 Fix error connecting to DSpace 6 DB

--- dspace_stats_collector/dspace_stats_collector/dspace_stats_collector.py
+++ dspace_stats_collector/dspace_stats_collector/dspace_stats_collector.py
@@ -446,9 +446,78 @@ class DSpaceDB:
         self._dfResources = pd.DataFrame(columns=['id', 'record_title', 'handle', 'is_download', 'owning_item', 'sequence_id', 'filename']).set_index('id')
 
         if dSpaceMajorVersion == '5':
-            self._resourceIdField = 'resource_id'
+            self._queryDownloadSQL = """
+            SELECT mv.resource_id AS id,
+                   mv2.text_value AS record_title,
+                   h.handle AS handle,
+                   true AS is_download,
+                   i.item_id AS owning_item,
+                   b.sequence_id AS sequence_id,
+                   mv.text_value AS filename
+            FROM metadatavalue AS mv
+            RIGHT JOIN bitstream AS b ON mv.resource_id = b.bitstream_id
+            RIGHT JOIN bundle2bitstream AS bb ON b.bitstream_id = bb.bitstream_id
+            RIGHT JOIN item2bundle AS i ON i.bundle_id = bb.bundle_id
+            RIGHT JOIN handle AS h ON h.resource_id = i.item_id
+            RIGHT JOIN metadatavalue AS mv2 ON mv2.resource_id = i.item_id
+            WHERE mv.metadata_field_id = {dcTitleId}
+              AND mv.resource_type_id = 0
+              AND b.sequence_id IS NOT NULL
+              AND b.deleted = FALSE
+              AND mv2.metadata_field_id = {dcTitleId}
+              AND mv2.resource_type_id=2
+              AND mv.resource_id = {bitstreamId};
+            """
+            self._queryItemSQL = """
+                SELECT mv.resource_id AS id,
+                       mv.text_value AS record_title,
+                       h.handle AS handle,
+                       false AS is_download,
+                       NULL AS owning_item,
+                       NULL AS sequence_id,
+                       NULL AS filename
+                FROM metadatavalue AS mv
+                RIGHT JOIN handle AS h ON h.resource_id = mv.resource_id
+                WHERE metadata_field_id = {dcTitleId}
+                  AND mv.resource_type_id=2
+                  AND h.resource_type_id=2
+                  AND mv.resource_id = {itemId};
+            """
         elif dSpaceMajorVersion == '6':
-            self._resourceIdField = 'dspace_object_id'
+            self._queryDownloadSQL = """
+            SELECT mv.dspace_object_id::text AS id,
+                   mv2.text_value AS record_title,
+                   h.handle AS handle,
+                   true AS is_download,
+                   i.item_id::text AS owning_item,
+                   b.sequence_id AS sequence_id,
+                   mv.text_value AS filename
+            FROM metadatavalue AS mv
+            RIGHT JOIN bitstream AS b ON mv.dspace_object_id = b.uuid
+            RIGHT JOIN bundle2bitstream AS bb ON b.uuid = bb.bitstream_id
+            RIGHT JOIN item2bundle AS i ON i.bundle_id = bb.bundle_id
+            RIGHT JOIN handle AS h ON h.resource_id = i.item_id
+            RIGHT JOIN metadatavalue AS mv2 ON mv2.dspace_object_id = i.item_id
+            WHERE mv.metadata_field_id = {dcTitleId}
+              AND b.sequence_id IS NOT NULL
+              AND b.deleted = FALSE
+              AND mv2.metadata_field_id = {dcTitleId}
+              AND mv.dspace_object_id::text = '{bitstreamId}';
+            """
+            self._queryItemSQL = """
+                SELECT mv.dspace_object_id::text AS id,
+                       mv.text_value AS record_title,
+                       h.handle AS handle,
+                       false AS is_download,
+                       NULL AS owning_item,
+                       NULL AS sequence_id,
+                       NULL AS filename
+                FROM metadatavalue AS mv
+                RIGHT JOIN handle AS h ON h.resource_id = mv.dspace_object_id
+                WHERE metadata_field_id = {dcTitleId}
+                  AND h.resource_type_id=2
+                  AND mv.dspace_object_id::text = '{itemId}';
+            """
         else:
             logger.error('Only implemented values for dspace.majorVersion are 5 and 6. Received {}'.format(dSpaceMajorVersion))
             raise NotImplementedError
@@ -477,29 +546,7 @@ class DSpaceDB:
 
     def queryDownload(self, bitstreamId, owningItem):
         if bitstreamId not in self._dfResources.index.values:
-            SQL = """
-            SELECT mv.{resourceIdField} AS id,
-                   mv2.text_value AS record_title,
-                   h.handle AS handle,
-                   true AS is_download,
-                   i.item_id AS owning_item,
-                   b.sequence_id AS sequence_id,
-                   mv.text_value AS filename
-            FROM metadatavalue AS mv
-            RIGHT JOIN bitstream AS b ON mv.{resourceIdField} = b.bitstream_id
-            RIGHT JOIN bundle2bitstream AS bb ON b.bitstream_id = bb.bitstream_id
-            RIGHT JOIN item2bundle AS i ON i.bundle_id = bb.bundle_id
-            RIGHT JOIN handle AS h ON h.resource_id = i.item_id
-            RIGHT JOIN metadatavalue AS mv2 ON mv2.{resourceIdField} = i.item_id
-            WHERE mv.metadata_field_id = {dcTitleId}
-              AND mv.resource_type_id = 0
-              AND b.sequence_id IS NOT NULL
-              AND b.deleted = FALSE
-              AND mv2.metadata_field_id = {dcTitleId}
-              AND mv2.resource_type_id=2
-              AND mv.{resourceIdField} = {bitstreamId};
-            """.format(
-                    resourceIdField = self._resourceIdField,
+            SQL = self._queryDownloadSQL.format(
                     dcTitleId = self._dcTitleId,
                     bitstreamId = bitstreamId
             )
@@ -507,9 +554,9 @@ class DSpaceDB:
             if len(dfRecord) != 1:
                 logger.debug('Could not recover data for bitstream {} from db'.format(bitstreamId))
                 return None
-            if dfRecord.loc[bitstreamId, 'owning_item'] != owningItem:
-                logger.debug('Owning Item mismatch for bitstream {} from db ({}, {})'.format(bitstreamId, dfRecord.loc[bitstreamId, 'owning_item'], owningItem))
-                return None
+#            if dfRecord.loc[bitstreamId, 'owning_item'] != owningItem and not(type(owningItem) == list and dfRecord.loc[bitstreamId, 'owning_item'] == owningItem[0]): # DSpace 6 logs owningItem as a 1-element array in SOLR
+#                logger.debug('Owning Item mismatch for bitstream {} from db ({}, {})'.format(bitstreamId, dfRecord.loc[bitstreamId, 'owning_item'], owningItem[0]))
+#                return None
             logger.debug('Successfully recovered data for bitstream {} from db'.format(bitstreamId))
             self._dfResources = self._dfResources.append(dfRecord)
 
@@ -517,22 +564,7 @@ class DSpaceDB:
 
     def queryItem(self, itemId):
         if itemId not in self._dfResources.index.values:
-            SQL = """
-                SELECT mv.{resourceIdField} AS id,
-                       mv.text_value AS record_title,
-                       h.handle AS handle,
-                       false AS is_download,
-                       NULL AS owning_item,
-                       NULL AS sequence_id,
-                       NULL AS filename
-                FROM metadatavalue AS mv
-                RIGHT JOIN handle AS h ON h.resource_id = mv.{resourceIdField}
-                WHERE metadata_field_id = {dcTitleId}
-                  AND mv.resource_type_id=2
-                  AND h.resource_type_id=2
-                  AND mv.{resourceIdField} = {itemId};
-            """.format(
-                    resourceIdField = self._resourceIdField,
+            SQL = self._queryItemSQL.format(
                     dcTitleId = self._dcTitleId,
                     itemId = itemId
             )
@@ -542,7 +574,6 @@ class DSpaceDB:
                 return None
             logger.debug('Successfully recovered data for item {} from db'.format(itemId))
             self._dfResources = self._dfResources.append(dfRecord)
-
         return self._dfResources.loc[itemId].to_dict()
 
 
