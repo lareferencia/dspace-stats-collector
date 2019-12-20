6bde64d982da38678576e2524afc39fa40c1ce78
Lautaro Matas <lmatas@gmail.com>
2019-12-20 15:59:38 +0100

    version update

--- dspace_stats_collector/__init__.py
+++ dspace_stats_collector/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """LA Referencia"""
 __email__ = 'lareferencia.dev@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 __name__ = "dspace_stats_collector"
 __package__ = "dspace_stats_collector"
