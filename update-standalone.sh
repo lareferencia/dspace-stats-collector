4b5d024e65582456e50ea67a343683bd895433a9
Lautaro Matas <lmatas@gmail.com>
2022-06-28 20:01:01 +0200

    update differential requirements in upgrade script

--- update-standalone.sh
+++ update-standalone.sh
@@ -8,13 +8,26 @@ echo "Updating dspace-stats-collector version"
 echo "Installing dspace-stats-collector package dependencies"
 rm -f $INSTALL_PATH/requirements.txt
 
+python_version = `$INSTALL_PATH/bin/python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/'`
+echo "Python version: $python_version"
+
+requirements_file="requirements.txt"
+
+echo "Downloading dspace-stats-collector python $python_version dependencies"
+if [ $python_version == "37" ]; then
+    requirements_file = "requirements-p37.txt"
+else
+    requirements_file = "requirements.txt"
+fi
+
 if [ -x "$(which curl)" ]; then
-  curl  https://raw.githubusercontent.com/lareferencia/dspace-stats-collector/master/requirements.txt -o $INSTALL_PATH/requirements.txt
+  curl  https://raw.githubusercontent.com/lareferencia/dspace-stats-collector/master/$requirements_file -o $INSTALL_PATH/requirements.txt
 else
     echo "Could not find curl, please install curl." >&2
     exit
 fi
 
+echo "Installing dspace-stats-collector package dependencies"
 $INSTALL_PATH/bin/pip install -r $INSTALL_PATH/requirements.txt
 
 echo "Removing dspace-stats-collector installed version"
