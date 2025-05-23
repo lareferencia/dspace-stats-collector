4f8a08a741e6b63c0cb97c7a45bf2b38564deecf
Lautaro Matas <lmatas@gmail.com>
2025-05-23 10:23:19 +0200

    update version

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/lareferencia/dspace-stats-collector',
-    version='0.7.6',
+    version='0.7.7',
     zip_safe=False,
 )
